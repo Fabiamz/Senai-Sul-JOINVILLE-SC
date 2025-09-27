774c511a48b83d93e561ad7d474d79466b34899f
Fabiamz <gustavojoaquimfabiam@gmail.com>
2025-09-26 22:20:46 -0300

    Atividade

--- PROFESSOR LUQUINHAS/Tutorial Pandas PROFESSOR LUQUINHAS/desafio26-09.py
+++ PROFESSOR LUQUINHAS/Tutorial Pandas PROFESSOR LUQUINHAS/desafio26-09.py
@@ -8,3 +8,7 @@ print(f"Média geral das notas: {media_geral}")
 menor_que_7 = df[df["Nota"] < 7].__len__()
 print("Alunos com nota menor que 7:")
 print(menor_que_7)  
+
+aluno_maior_de_18 = df[(df["Idade"] > 18) & (df["Nota"] > 7)]
+print("Aluno maior de 18 anos com nota maior que 7:")
+print(aluno_maior_de_18)
